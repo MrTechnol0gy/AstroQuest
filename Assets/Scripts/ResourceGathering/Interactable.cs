c85aad56e1b55073d970ddf75905dfd15ee908ad
Chris Schnurr <ctschnurr@hotmail.com>
2023-10-20 11:56:57 -0300

    Made a start on the resource collection system

---
+++ Assets/Scripts/ResourceGathering/Interactable.cs
@@ -0,0 +1,9 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+// THIS IS A COPY OF ROWAN'S CODE! DELETE THIS CLASS ONCE ROWAN'S CODE IS ADDED!
+public abstract class Interactable : MonoBehaviour
+{
+    public abstract void Activate();
+}

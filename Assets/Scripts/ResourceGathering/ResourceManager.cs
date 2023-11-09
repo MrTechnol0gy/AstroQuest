e5671ac3f5274cfe04ed96feb97e55dbca2659c1
Ryan Wohler <ryanwohler55@gmail.com>
2023-11-09 11:30:23 -0400

    updated ResourceGathering to prep for merge

--- Assets/Scripts/ResourceGathering/ResourceManager.cs
+++ Assets/Scripts/ResourceGathering/ResourceManager.cs
@@ -29,7 +29,6 @@ public class ResourceManager : MonoBehaviour
 
     public static int[] resourceInventory;
 
-    public static 
     // Start is called before the first frame update
     void Start()
     {
